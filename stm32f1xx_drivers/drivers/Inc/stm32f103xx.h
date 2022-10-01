2816c9426bc093d28d4d14c2c99e7957140500d9
Oğuz Kağan Yağlıoğlu <me@oguzkagan.xyz>
2022-10-01 22:10:41 +0300

    spi register definition added

--- stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
+++ stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
@@ -223,6 +223,22 @@ typedef struct {
 } EXTI_RegDef_t;
 
 /**
+ * peripheral register definition structure for the SPI
+ * */
+typedef struct {
+    volatile uint32_t SPI_CR1;          // SPI control register 1          (not used in I2S mode)   Address offset 0x00
+    volatile uint32_t SPI_CR2;          // SPI control register 2                                   Address offset 0x04
+    volatile uint32_t SPI_SR;           // SPI status register                                      Address offset 0x08
+    volatile uint32_t SPI_DR;           // SPI data register                                        Address offset 0x0C
+    volatile uint32_t SPI_CRCPR;        // SPI CRC polynomial register     (not used in I2S mode)   Address offset 0x10
+    volatile uint32_t SPI_RXCRCR;       // SPI RX CRC register             (not used in I2S mode)   Address offset 0x14
+    volatile uint32_t SPI_TXCRCR;       // SPI TX CRC register             (not used in I2S mode)   Address offset 0x18
+    volatile uint32_t SPI_I2SCFGR;      // SPI_I2S configuration register                           Address offset 0x1C
+    volatile uint32_t SPI_I2SPR;        // SPI_I2S prescaler register                               Address offset 0x20
+} SPI_RegDef_t;
+
+
+/**
  * peripheral register definition structure for the AFIO
  * */
 typedef struct {
@@ -247,6 +263,8 @@ typedef struct {
 #define RCC   ((RCC_RegDef_t *) RCC_BASE_ADDR)
 #define EXTI  ((EXTI_RegDef_t *) EXTI_BASE_ADDR)
 #define AFIO  ((AFIO_RegDef_t *) AFIO_BASE_ADDR)
+#define SPI1  ((SPI_RegDef_t *) SPI1_BASE_ADDR)
+#define SPI2  ((SPI_RegDef_t *) SPI2_BASE_ADDR)
 
 /*
  * Clock enable/disable macros for AFIO peripheral
